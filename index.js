720e4ec772d16a9d6849aa22063d0453a6bdc3bd
Pawel Student <pawelstude@gmail.com>
2020-08-19 20:39:04 +0200

    Fix join path and add console message after server is started

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const path = require("path");
 const port = 3000;
 
 const app = express();
-app.use(serveStatic(path.join(__dirname), 'files'))
-app.listen(port);
+
+app.use(serveStatic(path.join(__dirname, 'files')))
+app.listen(port, () => console.log(`Image server is run at http://localhost:${port}\nTo close use CTRL + C`));
