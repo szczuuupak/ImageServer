898d131694a2a98768c7913367e86cdef45930bc
Pawel Student <pawelstude@gmail.com>
2020-08-19 20:33:26 +0200

    Setup server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+const port = 3000;
+
+const app = express();
+app.use(serveStatic(path.join(__dirname), 'files'))
+app.listen(port);
